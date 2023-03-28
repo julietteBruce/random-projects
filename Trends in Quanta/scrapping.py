6732ccafb14aca223d5094406f430289ac7e5706
Juliette Bruce <jemmy.bruce@gmail.com>
2023-03-28 12:00:31 -0700

    updates

--- Trends in Quanta/scrapping.py
+++ Trends in Quanta/scrapping.py
@@ -1,16 +1,17 @@
+from typing import List, Dict, Literal
 from bs4 import BeautifulSoup
 import requests
 import re 
 import pandas as pd
 
-def get_webpage(url):
+def get_webpage(url: str):
 	req = requests.get(url)
 	return BeautifulSoup(req.content, 'html.parser')
 
-def unique_elements(input_list):
+def unique_elements(input_list: List) -> List:
 	return list(set(input_list)) 
 
-def get_href_links(webpage_soup):
+def get_href_links(webpage_soup) -> List[str]:
 	""" Returns list with all href links on page from URL."""
 	all_links = webpage_soup.find_all("a")
 	return [link.get('href') for link in all_links]
@@ -18,20 +19,20 @@ def get_href_links(webpage_soup):
 # webpage_soup = get_webpage('https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/')
 # print(get_href_links(webpage_soup))
 
-def get_href_links_url(url):
+def get_href_links_url(url: str) -> List[str]:
 	""" Returns list with all href links on page from URL."""
 	webpage_soup = get_webpage(url)
 	return get_href_links(webpage_soup)
 
 # print(get_href_links_url('https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/'))
 
-def remove_none_values_from_list(input_list):
+def remove_none_values_from_list(input_list: List) -> List:
 	return [elm for elm in input_list if elm is not None]
 
 # input_list = ['test', 'arxivtest', None, '2212testdog']
 # print(remove_none_values_from_list(input_list))
 
-def elmements_with_substring(input_list,desired_substring):
+def elmements_with_substring(input_list: List[str], desired_substring: str) -> List[str]:
 	"""Returns a list of elements from a list with a given substring."""
 	return [elm for elm in input_list if elm is not None and desired_substring in elm]
 
@@ -43,21 +44,20 @@ def elmements_with_substring(input_list,desired_substring):
 # desired_substring = 'arxiv'
 # print(elmements_with_substring(input_list,desired_substring))
 
-def has_substring_from_list(input_string,list_of_substrings):
+def has_substring_from_list(input_string: str, list_of_substrings: List[str]) -> bool:
 	for desired_substring in list_of_substrings:
 		if input_string is not None and desired_substring in input_string:
 			return True
 	return False
 
-def elements_without_substrings(list_of_links,unwanted_substrings):
+def elements_without_substrings(list_of_links: List[str], unwanted_substrings: List[str]) -> List[str]:
 	links_to_return = []
 	for link in list_of_links:
 		if not has_substring_from_list(link,unwanted_substrings):
 			links_to_return.append(link)
 	return links_to_return
 
-def get_quanta_links_from_archive_page(base_url,page_number):
-	print()
+def get_quanta_links_from_archive_page(base_url: str, page_number: int) -> List[str]:
 	list_of_links = get_href_links_url(base_url + 'page/' + str(page_number))
 	unwanted_substrings = ['/tag', '/authors/', 'http', '/privacy-policy', '/physics/', '/mathematics/', '/biology/', 
 							'/computer-science/', '/topics', '/archive/', '/topics','/about/', '/archive', '/contact-us/', 
@@ -71,7 +71,7 @@ def get_quanta_links_from_archive_page(base_url,page_number):
 # print(test)
 # print(len(test))
 
-def get_quanta_links_from_archive(base_url,last_page_number):
+def get_quanta_links_from_archive(base_url: str, last_page_number: int) -> List[str]:
 	links_to_return = []
 	for page_number in range(1,last_page_number+1):
 		print(page_number)
@@ -82,7 +82,7 @@ def get_quanta_links_from_archive(base_url,last_page_number):
 # print(test)
 # print(len(test))
 
-def get_certain_links(webpage_soup,desired_substring):
+def get_certain_links(webpage_soup, desired_substring: str) -> List[str]:
 	"""Returns a list of href links on the page from URL with given substring."""
 	href_links = get_href_links(webpage_soup)
 	return elmements_with_substring(href_links,desired_substring)
@@ -91,7 +91,7 @@ def get_certain_links(webpage_soup,desired_substring):
 # desired_substring = 'arxiv'
 # print(get_certain_links(webpage_soup,desired_substring))
 
-def get_certain_links_url(url,desired_substring):
+def get_certain_links_url(url: str, desired_substring: str) -> List[str]:
 	"""Returns a list of href links on the page from URL with given substring."""
 	href_links = get_href_links_url(url)
 	return elmements_with_substring(href_links,desired_substring)
@@ -100,7 +100,7 @@ def get_certain_links_url(url,desired_substring):
 # desired_substring = 'arxiv'
 # print(get_certain_links_url(url,desired_substring))
 
-def convert_arxiv_pdf_to_abs(url):
+def convert_arxiv_pdf_to_abs(url: str) -> str:
 	if url[-3:] == 'pdf':
 		arxiv_link_unedited = url[:-4]
 		broken_link = arxiv_link_unedited.split('pdf')
@@ -119,7 +119,7 @@ def convert_arxiv_pdf_to_abs(url):
 # url = 'https://arxiv.org/abs/0802.3361'
 # print(convert_arxiv_pdf_to_abs(url))
 
-def get_arxiv_links(webpage_soup):
+def get_arxiv_links(webpage_soup) -> List[str]:
 	links_containing_arxiv_substring = get_certain_links(webpage_soup,'arxiv')
 	unwanted_arxiv_type_sites = ['eartharxiv','search']
 	wanted_arxiv_links = elements_without_substrings(links_containing_arxiv_substring, unwanted_arxiv_type_sites)
@@ -137,7 +137,7 @@ def get_arxiv_links(webpage_soup):
 # webpage_soup = get_webpage('https://www.quantamagazine.org/mathematicians-find-a-new-class-of-digitally-delicate-primes-20210330/')
 # print(get_arxiv_links(webpage_soup))
 
-def get_arxiv_links_url(url):
+def get_arxiv_links_url(url: str) -> List[str]:
 	webpage_soup = get_webpage(url)
 	return get_arxiv_links(webpage_soup)
 
@@ -149,7 +149,7 @@ def get_arxiv_links_url(url):
 # url = 'https://www.quantamagazine.org/scientists-unravel-how-the-tonga-volcano-caused-worldwide-tsunamis-20220413/'
 # print(get_arxiv_links_url(url))
 
-def get_MSC_from_arxiv(webpage_soup):
+def get_MSC_from_arxiv(webpage_soup) -> List[str]:
 	msc_tbs = webpage_soup.find_all("td", {"class": "msc-classes"})
 	return [elm.getText() for elm in msc_tbs]
 
@@ -160,7 +160,7 @@ def get_MSC_from_arxiv(webpage_soup):
 # webpage_soup = get_webpage('https://arxiv.org/abs/2303.11723')
 # print(get_MSC_from_arxiv(webpage_soup))
 
-def get_MSC_from_arxiv_url(url):
+def get_MSC_from_arxiv_url(url: str) -> List[str]:
 	webpage_soup = get_webpage(url)
 	return get_MSC_from_arxiv(webpage_soup)
 
@@ -171,7 +171,7 @@ def get_MSC_from_arxiv_url(url):
 # url = 'https://arxiv.org/abs/2303.11723'
 # print(get_MSC_from_arxiv_url(url))
 
-def split_MSC_from_arxiv(webpage_soup):
+def split_MSC_from_arxiv(webpage_soup) -> Dict[str, List]:
 	dictionary_MSC = {'primary': [], 'secondary': []}
 	list_MSC = get_MSC_from_arxiv(webpage_soup)
 	if list_MSC:
@@ -189,7 +189,7 @@ def split_MSC_from_arxiv(webpage_soup):
 # webpage_soup = get_webpage('https://arxiv.org/abs/2303.11723')
 # print(split_MSC_from_arxiv(webpage_soup))
 
-def split_MSC_from_arxiv_url(url):
+def split_MSC_from_arxiv_url(url: str) -> Dict[str, List]:
 	webpage_soup = get_webpage(url)
 	return split_MSC_from_arxiv(webpage_soup)
 
@@ -200,21 +200,21 @@ def split_MSC_from_arxiv_url(url):
 # url = 'https://arxiv.org/abs/2303.11723'
 # print(split_MSC_from_arxiv_url(url))
 
-def get_primarySubject_from_arxiv(webpage_soup):
+def get_primarySubject_from_arxiv(webpage_soup) -> List[str]:
 	sub_tbs = webpage_soup.find_all("span", {"class": "primary-subject"})
 	return [elm.getText() for elm in sub_tbs]
 
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_primarySubject_from_arxiv(webpage_soup))
 
-def get_primarySubject_from_arxiv_url(url):
+def get_primarySubject_from_arxiv_url(url: str) -> List[str]:
 	webpage_soup = get_webpage(url)
 	return get_primarySubject_from_arxiv(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_primarySubject_from_arxiv_url(url))
 
-def get_subjects_from_arxiv(webpage_soup):
+def get_subjects_from_arxiv(webpage_soup) -> List[str]:
 	sub_tbs = webpage_soup.find_all("td", {"class": "subjects"})
 	subs_unformated = [elm.getText() for elm in sub_tbs][0].split("; ")
 	return [elm.lstrip() for elm in subs_unformated]
@@ -222,14 +222,14 @@ def get_subjects_from_arxiv(webpage_soup):
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_primarySubject_from_arxiv(webpage_soup))
 
-def get_subjects_from_arxiv_url(url):
+def get_subjects_from_arxiv_url(url: str) -> List[str]:
 	webpage_soup = get_webpage(url)
 	return get_subjects_from_arxiv(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_primarySubject_from_arxiv_url(url))
 
-def get_secondary_subjects_from_arxiv(webpage_soup):
+def get_secondary_subjects_from_arxiv(webpage_soup) -> List[str]:
 	primary_subject = get_primarySubject_from_arxiv(webpage_soup)
 	all_subjects = get_subjects_from_arxiv(webpage_soup)
 	all_subjects.remove(primary_subject[0])
@@ -238,7 +238,7 @@ def get_secondary_subjects_from_arxiv(webpage_soup):
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_secondary_subjects_from_arxiv(webpage_soup))
 
-def get_secondary_subjects_from_arxiv_url(url):
+def get_secondary_subjects_from_arxiv_url(url: str) -> List[str]:
 	webpage_soup = get_webpage(url)
 	return get_secondary_subjects_from_arxiv(webpage_soup)
 
@@ -249,7 +249,7 @@ def get_secondary_subjects_from_arxiv_url(url):
 # print(get_secondary_subjects_from_arxiv('https://arxiv.org/abs/2303.11428'))
 # print(get_secondarySubjects_from_arxiv('https://arxiv.org/abs/2212.11097'))
 
-def get_author_quanta_article(webpage_soup):
+def get_author_quanta_article(webpage_soup) -> List[str]:
 	author_div = webpage_soup.find_all("div", {"class": "h3t mv05"})
 	list_of_authors = unique_elements([elm.getText() for elm in author_div])
 	return [author.replace('By ', '') for author in list_of_authors]
@@ -257,14 +257,14 @@ def get_author_quanta_article(webpage_soup):
 # webpage_soup = get_webpage('https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/')
 # print(get_author_quanta_article(webpage_soup))
 
-def get_author_quanta_article_url(url):
+def get_author_quanta_article_url(url: str) -> List[str]:
 	webpage_soup = get_webpage(url)
 	return get_author_quanta_article(webpage_soup)
 
 # url = 'https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/'
 # print(get_author_quanta_article_url(url))
 
-def get_pub_date_quanta_article(webpage_soup):
+def get_pub_date_quanta_article(webpage_soup) -> str:
 	date_element = (webpage_soup.find_all("meta", {"property": "article:published_time"}))[0]
 	pub_data_time = date_element['content']
 	return pub_data_time[:10]
@@ -272,14 +272,14 @@ def get_pub_date_quanta_article(webpage_soup):
 # webpage_soup = get_webpage('https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/')
 # print(get_pub_date_quanta_article(webpage_soup))
 
-def get_pub_date_quanta_article_url(url):
+def get_pub_date_quanta_article_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_pub_date_quanta_article(webpage_soup)
 
 # url = 'https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/'
 # print(get_pub_date_quanta_article_url(url))
 
-def get_title_quanta_article(webpage_soup):
+def get_title_quanta_article(webpage_soup) -> str:
 	title_element = (webpage_soup.find_all("meta", {"property": "og:title"}))[0]
 	title_with_branding = title_element['content']
 	title = (title_with_branding.split(" | "))[0]
@@ -288,14 +288,14 @@ def get_title_quanta_article(webpage_soup):
 # webpage_soup = get_webpage('https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/')
 # print(get_title_quanta_article(webpage_soup))
 
-def get_title_quanta_article_url(url):
+def get_title_quanta_article_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_title_quanta_article(webpage_soup)
 
 # url = 'https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/'
 # print(get_title_quanta_article_url(url))
 
-def get_author_arxiv_listing(webpage_soup):
+def get_author_arxiv_listing(webpage_soup) -> str:
 	author_div = webpage_soup.find_all("meta", {"name": "citation_author"})
 	list_of_authors = unique_elements([author['content'] for author in author_div])
 	return list_of_authors
@@ -303,56 +303,56 @@ def get_author_arxiv_listing(webpage_soup):
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_author_arxiv_listing(webpage_soup))
 
-def get_author_arxiv_listing_url(url):
+def get_author_arxiv_listing_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_author_arxiv_listing(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_author_arxiv_listing_url(url))
 
-def get_title_arxiv_listing(webpage_soup):
+def get_title_arxiv_listing(webpage_soup) -> str:
 	title_div = (webpage_soup.find_all("meta", {"name": "citation_title"}))[0]
 	return title_div['content']
 
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_title_arxiv_listing(webpage_soup))
 
-def get_title_arxiv_listing_url(url):
+def get_title_arxiv_listing_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_title_arxiv_listing(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_title_arxiv_listing_url(url))
 
-def get_original_date_arxiv_listing(webpage_soup):
+def get_original_date_arxiv_listing(webpage_soup) -> str:
 	date_div = (webpage_soup.find_all("meta", {"name": "citation_date"}))[0]
 	return date_div['content']
 
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_original_date_arxiv_listing(webpage_soup))
 
-def get_original_date_arxiv_listing_url(url):
+def get_original_date_arxiv_listing_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_original_date_arxiv_listing(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_original_date_arxiv_listing_url(url))
 
-def get_recent_date_arxiv_listing(webpage_soup):
+def get_recent_date_arxiv_listing(webpage_soup) -> str:
 	date_div = (webpage_soup.find_all("meta", {"name": "citation_online_date"}))[0]
 	return date_div['content']
 
 # webpage_soup = get_webpage('https://arxiv.org/abs/2212.11097')
 # print(get_recent_date_arxiv_listing(webpage_soup))
 
-def get_recent_date_arxiv_listing_url(url):
+def get_recent_date_arxiv_listing_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_recent_date_arxiv_listing(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_recent_date_arxiv_listing_url(url))
 
-def get_id_arxiv_listing(webpage_soup):
+def get_id_arxiv_listing(webpage_soup) -> str:
 	id_div = (webpage_soup.find_all("meta", {"name": "citation_arxiv_id"}))[0]
 	return id_div['content']
 
@@ -360,14 +360,14 @@ def get_id_arxiv_listing(webpage_soup):
 # webpage_soup = get_webpage('https://arxiv.org/abs/1201.6644')
 # print(get_id_arxiv_listing(webpage_soup))
 
-def get_id_arxiv_listing_url(url):
+def get_id_arxiv_listing_url(url: str) -> str:
 	webpage_soup = get_webpage(url)
 	return get_id_arxiv_listing(webpage_soup)
 
 # url = 'https://arxiv.org/abs/2212.11097'
 # print(get_id_arxiv_listing_url(url))
 
-def process_arxiv_listing(url):
+def process_arxiv_listing(url: str) -> Dict[str, Dict]:
 	webpage_soup = get_webpage(url)
 	output_dictionary = {
 	'arxiv_id': get_id_arxiv_listing(webpage_soup),
@@ -389,7 +389,7 @@ def process_arxiv_listing(url):
 # url = 'https://arxiv.org/abs/2303.11723'
 # print(process_arxiv_listing(url))
 
-def quanta_article_overview(webpage_soup):
+def quanta_article_overview(webpage_soup) -> Dict[str, Dict]:
 	output_dictionary = {
 	'quanta_author' : get_author_quanta_article(webpage_soup),
 	'quanta_pub_date': get_pub_date_quanta_article(webpage_soup),
@@ -400,14 +400,14 @@ def quanta_article_overview(webpage_soup):
 # webpage_soup = get_webpage('https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/')
 # print(quanta_article_overview(webpage_soup))
 
-def quanta_article_overview_url(url):
+def quanta_article_overview_url(url: str) -> Dict[str, Dict]:
 	webpage_soup = get_webpage(url)
 	return quanta_article_overview(webpage_soup)
 
 # url = 'https://www.quantamagazine.org/long-sought-math-proof-unlocks-more-mysterious-modular-forms-20230309/'
 # print(quanta_article_overview_url(url))
 
-def process_quanta_article(url):
+def process_quanta_article(url: str) -> Dict[str, Dict]:
 	webpage_soup = get_webpage(url)
 	quanta_article_dictionary = quanta_article_overview(webpage_soup)
 	arxiv_links = get_arxiv_links(webpage_soup)
@@ -424,7 +424,7 @@ def process_quanta_article(url):
 # print(process_quanta_article(url))
 
 
-def process_quanta_archive(base_url,page_number):
+def process_quanta_archive(base_url: str, page_number: int) -> Dict[str, List]:
 	list_of_quanta_links = get_quanta_links_from_archive(base_url,page_number)
 	output_arxiv_list = []
 	output_quanta_list = []
@@ -436,7 +436,7 @@ def process_quanta_archive(base_url,page_number):
 		output_quanta_list.append(articlie_dictionay['quanta_article_dictionary'])
 	return {'output_arxiv_list': output_arxiv_list, 'output_quanta_list': output_quanta_list}
 
-def process_quanta_archive_page(base_url,page_number):
+def process_quanta_archive_page(base_url: str, page_number: int) -> Dict[str, Dict]:
 	list_of_quanta_links = get_quanta_links_from_archive_page(base_url,page_number)
 	output_arxiv_list = []
 	output_quanta_list = []
@@ -455,7 +455,7 @@ def process_quanta_archive_page(base_url,page_number):
 # print(test['output_arxiv_list'])
 # print(test['output_quanta_list'])
 
-def count_nonempty_lists(list_of_lists):
+def count_nonempty_lists(list_of_lists: List[List]) -> int:
 	running_cout_of_nonempty_lists = 0
 	for list1 in list_of_lists:
 		if list1:
@@ -465,7 +465,7 @@ def count_nonempty_lists(list_of_lists):
 # test = [[], [1,2], [], [2,3]]
 # print(count_nonempty_lists(test))
 
-def flatten_list(list_of_lists):
+def flatten_list(list_of_lists: List[List]) -> List:
 	return [elm for list1 in list_of_lists for elm in list1]
 
 # test = [[], [1], [], [2,3]]
